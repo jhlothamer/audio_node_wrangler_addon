5aca9706f0c20116ab05bef0ade21ca9b0d8a77c
Jason Lothamer <jhlothamer@gmail.com>
2024-05-13 17:38:02 -0400

    move demo folder to be under addon folder

---
+++ addon_project/addons/audio_node_wrangler/demo/game_consts.gd
@@ -0,0 +1,9 @@
+class_name GameConsts
+extends Object
+
+
+const SCENE_PATH_TITLE = "res://addons/audio_node_wrangler/demo/demo_title.tscn"
+const SCENE_PATH_CREDITS = "res://addons/audio_node_wrangler/demo/credits.tscn"
+const SCENE_PATH_QUESTION = "res://addons/audio_node_wrangler/demo/quiz_question.tscn"
+const SCENE_PATH_SCORE = "res://addons/audio_node_wrangler/demo/score.tscn"
+
--- addon_project/demo/game_consts.gd
+++
@@ -1,9 +0,0 @@
-class_name GameConsts
-extends Object
-
-
-const SCENE_PATH_TITLE = "res://demo/demo_title.tscn"
-const SCENE_PATH_CREDITS = "res://demo/credits.tscn"
-const SCENE_PATH_QUESTION = "res://demo/quiz_question.tscn"
-const SCENE_PATH_SCORE = "res://demo/score.tscn"
-
